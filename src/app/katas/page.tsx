fcf7c1cfaacbda4ca75c1a6f6a0f0841a4015a68
asazycat <asazycat@example.com>
2023-08-01 15:18:05 +0100

    list of placeholder data

---
+++ src/app/katas/page.tsx
@@ -0,0 +1,34 @@
+import Header from "../components/Header"
+import ProfileDd from "../components/ProfileDd"
+import SignIn from "../components/SignIn"
+async function getData() {
+    const res = await fetch('https://nc-news-render.onrender.com/api/articles')
+
+   
+    if (!res.ok) {
+     
+      throw new Error('Failed to fetch data')
+    }
+   
+    return res.json()
+  }
+
+
+
+export default async function katas() {
+
+
+    const data = await getData()
+ 
+        
+    return (
+        <div className="my-10 w-5/6 mx-auto" >
+        
+        {/* <ProfileDd/> */}
+        {/* <SignIn/> */}
+    <h1 className="">List of Katas</h1>
+<ul className = "flex flex-col justify-center items-center">{data.articles.map((element)=> {return <li className="m-5 bg-grey-400  p-3 border-0 rounded w-full hover:bg-grey-200 transition ease-in-out">{element.title}</li>})}</ul>
+</div>
+
+    )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from './components/Header'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -16,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-grey-500 text-grey-100`}>{children}</body>
+      <body className={`${inter.className} bg-grey-500 text-grey-100`}><Header/> {children}</body>
     </html>
   )
 }
