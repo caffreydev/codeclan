5b8ac78f0e6ebaecccedd5a0dcd09dfaef3d6eef
jetakazono <jessica.takazono@gmail.com>
2023-08-01 02:12:16 +0100

    fix: navbar with tailwind theme

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,77 +2,63 @@
 
 import Image from 'next/image'
 import Link from 'next/link'
-import { useState } from 'react'
+import { MouseEventHandler, useState } from 'react'
 
 export default function Header() {
-    const [dropdown, setDropdown] = useState(false)
+  const [isOpen, setIsOpen] = useState(false)
 
-    const handleClick = () => {
-        setDropdown((currValeu) => {
-            return !currValeu
-        })
-    }
+  const handleToggleMenu = () => setIsOpen(!isOpen)
 
   return (
+    <nav className="bg-grey-400">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto">
+        <Link href="/" className="flex items-center">
+            <span className="self-center text-2xl font-semibold whitespace-nowrap text-grey-100 hover:text-primary p-4">CodeClan</span>
+        </Link>
+        <div className="flex items-center md:order-2 relative mr-4">
 
-<nav className="bg-dark-grey">
-  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-  <a href="/" className="flex items-center">
-      <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">CodeClan</span>
-  </a>
-  <div className="flex items-center md:order-2 relative">
-      <button onClick={handleClick} type="button" className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
-        <span className="sr-only">Open user menu</span>
-        <Image className="rounded-full" src="/profile-avatar.png" width={35} height={35} alt="user photo"/>
-      </button>
-      {/* <!-- Dropdown menu --> */}
+          {/* <!-- Dropdown trigger / Avatar --> */}
+          <button onClick={handleToggleMenu} type="button" className="flex mr-3 text-sm bg-grey-300 rounded-full md:mr-0 focus:ring-2 focus:ring-primary hover:opacity-80" id="user-menu-button">
+            <span className="sr-only">Open user menu</span>
+            <Image className="rounded-full" src="/profile-avatar.png" width={35} height={35} alt="user photo"/>
+          </button>
 
-      <div className={`${dropdown ? "" : "hidden"} z-50 top-12 right-0 absolute my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600]`} id="user-dropdown">
-        <div className="px-4 py-3">
-          <span className="block text-sm text-gray-900 dark:text-white">jessie</span>
-          <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">jessie@codeclan.com</span>
+          {/* <!-- Dropdown content --> */}
+          <div onClick={handleToggleMenu} onMouseLeave={()=>setIsOpen(false)} className={`${isOpen ? "opacity-100 top-0" : "opacity-0 -top-4 pointer-events-none"} transition-all z-50 pt-10 right-0 absolute`}>
+            <div className={`text-base list-none divide-y divide-grey-400 rounded-lg shadow-xl bg-grey-300`} id="user-dropdown">
+              <div className="px-4 py-3">
+                <span className="block text-sm text-primary">jessie</span>
+                <span className="block text-sm text-grey-200 truncate">jessie@codeclan.com</span>
+              </div>
+              <ul className="py-2">
+                <li>
+                  <Link href="#" className="block px-4 py-2 text-sm hover:text-primary hover:bg-grey-400 transition">Profile</Link>
+                </li>
+                <li>
+                  <Link href="#" className="block px-4 py-2 text-sm hover:text-primary hover:bg-grey-400 transition">Dashboard</Link>
+                </li>
+                <li>
+                  <Link href="#" className="block px-4 py-2 text-sm hover:text-primary hover:bg-grey-400 transition">Settings</Link>
+                </li>
+                <li>
+                  <Link href="#" className="block px-4 py-2 text-sm hover:text-primary hover:bg-grey-400 transition">Sign out</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          
         </div>
-        <ul className="py-2" aria-labelledby="user-menu-button">
-          <li>
-            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
-          </li>
-          <li>
-            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
-          </li>
-          <li>
-            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
-          </li>
-          <li>
-            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
-          </li>
-        </ul>
+        <div className="items-center justify-between flex" id="navbar-user">
+          <ul className="flex font-medium p-4 rounded-lg space-x-8">
+            <li>
+              <Link href={'/'} className='hover:text-primary border-b-2 border-b-transparent hover:border-b-primary py-5 transition'>Home</Link>
+            </li>
+            <li>
+              <Link href={'/katas'} className='hover:text-primary border-b-2 border-b-transparent hover:border-b-primary py-5 transition'>Katas</Link>
+            </li>
+          </ul>
+        </div>
       </div>
-      <button data-collapse-toggle="navbar-user" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
-        <span className="sr-only">Open main menu</span>
-        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-        </svg>
-    </button>
-  </div>
-  <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
-    <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:border-0 ">
-      <li>
-        <Link href={'/'}>Home</Link>
-      </li>
-      <li>
-        <Link href={'/katas'}>Katas</Link>
-      </li>
-    </ul>
-  </div>
-  </div>
-</nav>
-
-
-
-
-    
-
-
-
+    </nav>
   )
 }
