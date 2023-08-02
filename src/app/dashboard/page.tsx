840b90fe52567811d3a7d0d9b68736668ca80cb5
Kc Tan <kctan@Kcs-MacBook-Air.local>
2023-08-02 11:09:16 +0100

    Commiting for merge - dashboard.v2 semi complete

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,31 +3,39 @@ import kataRequests from '@/sample-data/requests';
 import lfp from '@/sample-data/LfP';
 import Image from 'next/image';
 import Button from '../function/button';
+import group from '../function/groupByKata';
 
 type pageProps = {};
 
 const page:React.FC<pageProps> = () => {
+  const groupedByKata = group(kataRequests);
+
   return (
     <div className='max-w-screen-xl w-5/6 border-1px mx-auto py-10 px-2'>
       <h2 className='text-4xl font-bold mb-6'>Dashboard</h2>
       <h3 className='text-3xl font-bold mb-3 my-1'>Requests</h3>
       <hr className='border' />
-      <ul className='my-5'>
-        {kataRequests.map((request, i) => {
-          return <li key={request.reqId} className={`${i % 2 !== 1 ? 'bg-grey-400' : ''} flex p-3 box-border items-center`}>
-            <p className='inline flex-grow text-grey-150'>
-              <span className='cursor-pointer hover:text-teal-600 transition ease-in-out '>
-              <Image className='inline mr-3' src='/profile-avatar.png' alt='profile avatar' width={35} height={35}/>
-              {request.sender}</span> wants to do {" "}
-              <strong className='inline-block cursor-pointer text-grey-100 hover:text-teal-600 transition ease-in-out '>{request.title} </strong> 
-              {" "} with you
-            </p>
-            <Button style='mx-3 bg-primary border-0'>
+      <ul className='my-5 grid-cols-2 auto-rows-auto'>
+        {/* {groupedByKata.map((request, i) => {
+          return <li key={request.title} className={`${i % 2 !== 1 ? 'bg-grey-400' : ''} flex p-3 box-border items-center`}>
+            <Button style={`${i % 2 !== 1 ? 'bg-grey-400' : ''} mx-3 border-0`}>
               Accept
             </Button>
             <Button>
               Decline
             </Button>
+          </li>
+        })} */}
+        {Object.entries(groupedByKata).map(pair => {
+          return <li className='col-span-1 border-2 p-3 my-3 '>
+            <h4>{pair[0]}</h4>
+            <ul>
+              {pair[1].map(requestObj => {
+                return <li>
+                  {requestObj.sender}
+                </li>
+              })}
+            </ul>
           </li>
         })}
       </ul>
