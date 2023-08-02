35ff0be29a04e8ff84ba21eaf73824a56e838c1a
Damian <damiangildea12@gmail.com>
2023-08-02 16:10:19 +0100

    Links to inbox and specific messages

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import kataRequests from '@/sample-data/requests'; 
+import React from 'react';
 import Button from '../function/button';
-import group from '../function/groupByKata';
 import RequestList from './RequestList';
 import JoinList from './JoinList';
+import Link from 'next/link';
+
 
 
 type pageProps = {};
@@ -13,7 +13,9 @@ const page:React.FC<pageProps> = () => {
   return (
     <div className='max-w-screen-xl w-5/6 border-1px mx-auto py-10 px-2'>
       <h2 className='text-4xl font-bold mb-6'>Dashboard</h2>
-      <Button style="bg-primary border-0">Inbox</Button>
+      <Link href='/inbox'>
+          <Button style='bg-primary border-0'>Inbox</Button>
+      </Link>
       <h3 className='text-3xl font-bold mb-3 my-1'>Requests</h3>
       <hr className='border' />
     <RequestList />
