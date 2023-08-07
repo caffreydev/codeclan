694adb41184c3df56c7aea99c0745d6a3b044c85
asazycat <asazycat@example.com>
2023-08-07 12:46:59 +0100

    deleted unused lines

--- src/app/components/PairRequest.tsx
+++ src/app/components/PairRequest.tsx
@@ -1,14 +1,7 @@
 import {auth, firestore }from "../../firebase/firebase"
 import { collection, setDoc,doc , deleteDoc} from "firebase/firestore";
 import { useAuthState } from 'react-firebase-hooks/auth';
-import UserObjProps from "./UserObj";
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  messagingSenderId:process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  apiId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
-};
+
 
 
 
