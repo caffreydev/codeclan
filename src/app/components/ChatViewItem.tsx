c1d09475e4a7a4be21e57985bf1e485054a6c7b9
joemccaffrey-dev <joe.mccaffrey@hotmail.co.uk>
2023-08-07 16:36:03 +0100

    builds out message and chat functionality

--- src/app/components/ChatViewItem.tsx
+++ src/app/components/ChatViewItem.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import { Message } from '.';
+import { dateFromFirebaseTimestamp } from '@/Utils/dateFromFirebaseTimestamp';
 
 type ChatViewItemProps = {
   message: Message;
+  userId: string;
 };
 
-const ChatViewItem: React.FC<ChatViewItemProps> = ({ message }) => {
-  return <div>{message.body}</div>;
+const ChatViewItem: React.FC<ChatViewItemProps> = ({ message, userId }) => {
+  const selfMessage = message.fromId === userId;
+
+  return (
+    <div className={`mb-3 box-border rounded-2xl border p-3  ${selfMessage ? 'bg-white text-black' : 'bg-blue-500'}`}>
+      <h3 className='text-2xl'>{message.subject}</h3>
+      <p className={`text-sm italic ${selfMessage ? 'text-gray-500' : 'text-gray-300'}`}>
+        {selfMessage ? 'Sent' : 'Received'} at {dateFromFirebaseTimestamp(message.timeStamp)}
+      </p>
+      <br />
+      <p>{message.body}</p>
+    </div>
+  );
 };
 export default ChatViewItem;
---
+++ src/types/firestoreTypes.ts
@@ -0,0 +1,20 @@
+import { Timestamp } from 'firebase/firestore';
+
+export interface Message {
+  body: string;
+  fromDisplay: string;
+  fromId: string;
+  id: string;
+  subject: string;
+  timeStamp: Timestamp;
+  toDisplay: string;
+  toId: string;
+}
+
+export interface User {
+  userId: string;
+  displayName: string;
+  photoUrl: string;
+  joinTime: Timestamp;
+  completedKatas: number[];
+}
