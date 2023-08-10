f0260b338ed3de187b8e06b1f7ba5941376377a3
joemccaffrey-dev <joe.mccaffrey@hotmail.co.uk>
2023-08-10 14:15:05 +0100

    removes unused broken redux slices

--- src/redux/features/currentKata-slice.tsx
+++ src/redux/features/currentKata-slice.tsx
@@ -1,30 +1,30 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+// import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-type InitialState = {
-  value: CurrentKataState;
-};
+// type InitialState = {
+//   value: CurrentKataState;
+// };
 
-type CurrentKataState = {
-  kata: string;
-};
+// type CurrentKataState = {
+//   kata: string;
+// };
 
-const initialState = {
-  value: { kata: '' } as CurrentKataState,
-} as InitialState;
+// const initialState = {
+//   value: { kata: '' } as CurrentKataState,
+// } as InitialState;
 
-export const currentKataState = createSlice({
-  name: 'authModalState',
-  initialState,
-  reducers: {
-    setCurrentKata: (state, action: PayloadAction) => {
-      return {
-        value: {
-          kata: action.payload,
-        },
-      };
-    },
-  },
-});
+// export const currentKataState = createSlice({
+//   name: 'authModalState',
+//   initialState,
+//   reducers: {
+//     setCurrentKata: (state, action: PayloadAction) => {
+//       return {
+//         value: {
+//           kata: action.payload,
+//         },
+//       };
+//     },
+//   },
+// });
 
-export const { setCurrentKata } = currentKataState.actions;
-export default currentKataState.reducer;
+// export const { setCurrentKata } = currentKataState.actions;
+// export default currentKataState.reducer;
