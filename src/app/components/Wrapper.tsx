dfd1bd9306c409210192bb3fd2c8713086b791dd
joemccaffrey-dev <joe.mccaffrey@hotmail.co.uk>
2023-08-10 14:49:17 +0100

    fixes remaining errors

--- src/app/components/Wrapper.tsx
+++ src/app/components/Wrapper.tsx
@@ -1,10 +1,3 @@
-type WrapperProps = {
-  className: string;
-  children: any;
-};
-
-const Wrapper: React.FC<WrapperProps> = ({ children, className }) => {
+export default function Wrapper({ children, className }: any) {
   return <div className={`mx-auto w-full max-w-[87rem] px-4 sm:px-6 md:px-8 ${className}`}>{children}</div>;
-};
-
-export default Wrapper;
+}
